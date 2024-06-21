625a0f8d6490af44079f2e178edc121333abdd8b
TaThai <thaitv225@gmail.com>
2024-06-21 21:12:38 +0700

    redeploy

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
-
+app.get('/', (req, res) => {
+    res.send('server work!');
+  });
+  
 
 
 app.use('/register', require('./routes/register.route'));
